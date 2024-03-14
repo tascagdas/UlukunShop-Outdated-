40c40ad4ee41bf999d1b722269dcc59ba3a9faf0
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-03-14 16:00:38 +0300

    route yapısı eklenmeye başlandı fakat bir sorun fark ettim.
    Bu sorunu güncel angular versiyonu ile aşabilmem için projenin deadline'ını sıkıntıya sokabilir. bu sebeple süre avantajı sağlıyacağı için angular'ın versiyonunu düşürme kararı aldım. bir sonraki commit onun üzerine olacak

--- UlukunShop.UI/src/app/admin/components/order/order.module.ts
+++ UlukunShop.UI/src/app/admin/components/order/order.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {RouterModule} from "@angular/router";
+import {OrderComponent} from "./order.component";
 
 
 
 @NgModule({
   declarations: [],
   imports: [
+    RouterModule.forChild([
+      {path:"",component:OrderComponent}
+    ]),
     CommonModule
   ]
 })
