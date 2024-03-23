69efff3b7502bca2fd51491edecf85e2843c83d3
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-03-23 17:36:23 +0300

    angular kisminda validasyon islemleri icin hem backendden gelen response icin hemde frontend temelli basit bir validasyon islemi saglandi. bir sonraki commit api icin olan validasyon

--- UlukunShop.UI/src/app/admin/components/products/create/create.component.ts
+++ UlukunShop.UI/src/app/admin/components/products/create/create.component.ts
@@ -3,7 +3,7 @@ import {ProductService} from "../../../../services/common/models/product.service
 import {Create_Product} from "../../../../contracts/product";
 import {BaseComponent, SpinnerType} from "../../../../base/base.component";
 import {NgxSpinnerService} from "ngx-spinner";
-import {AlertifyService, MessageType} from "../../../../services/admin/alertify.service";
+import {AlertifyService, MessageType, Position} from "../../../../services/admin/alertify.service";
 
 @Component({
   selector: 'app-create',
@@ -25,9 +25,32 @@ export class CreateComponent extends BaseComponent implements OnInit {
     create_product.stock=parseInt(stock.value);
     create_product.price=parseFloat(price.value);
 
+    if (!name.value){
+      this.alertify.message("Lutfen urun adini giriniz",{
+        messageType:MessageType.Warning,
+        position:Position.TopRight
+      });
+      return;
+    }
+
+    if (parseInt(stock.value)<0) {
+      this.alertify.message("Lutfen stok bilgisini dogru giriniz",{
+        messageType:MessageType.Warning,
+        position:Position.TopRight
+      });
+      return;
+    }
+
+
     this.productService.create(create_product,()=> {
       this.hideSpinner(SpinnerType.BallAtom)
       this.alertify.message("Successfully created",{dismissOthers:true,messageType:MessageType.Success});
+    },errorMessage => {
+      this.alertify.message(errorMessage,{
+        dismissOthers:true,
+        messageType:MessageType.Error,
+        position:Position.TopRight
+      })
     });
   }
 
--- UlukunShop.UI/src/app/services/common/models/product.service.ts
+++ UlukunShop.UI/src/app/services/common/models/product.service.ts
@@ -1,20 +1,32 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClientService} from "../http-client.service";
 import {Create_Product} from "../../../contracts/product";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor(private _httpClientService:HttpClientService) { }
-  create(product: Create_Product,successCallBack?:any){
-this._httpClientService.post({
-  controller:"products"
-},product).subscribe(
-  response=>{
-    successCallBack();
+  constructor(private _httpClientService: HttpClientService) {
   }
-)
+
+  create(product: Create_Product, successCallBack?: any,errorCallBack?:any) {
+    this._httpClientService.post({
+      controller: "products"
+    }, product).subscribe(
+      response => {
+        successCallBack();
+      }, (errorResponse: HttpErrorResponse) => {
+        const _error: Array<{ key: string, value: Array<string> }> = errorResponse.error;
+        let message=""
+        _error.forEach((v,index)=>{
+          v.value.forEach((_v,_index)=>{
+            message+=`${_v}<br>`;
+          });
+        });
+        errorCallBack(message);
+
+      });
   }
 }
