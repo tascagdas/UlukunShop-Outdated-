145513bff79e123acc50bf899206cc6722af873f
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-04-01 16:30:27 +0300

    Angular kisminda reaktif form olusturuldu ve validasyonlar ve errorlar ayarlandi. submit edilmesi henuz yok. angular kismindada bir entity olusturulup backende oyle gonderilecek.

--- UlukunShop.UI/src/app/ui/components/register/register.component.ts
+++ UlukunShop.UI/src/app/ui/components/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {
   }
 
+  frm: FormGroup;
+
+  ngOnInit(): void {
+    this.frm = this.formBuilder.group({
+      firstName: ['', [Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(50)
+      ]],
+      lastName: ['', [Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(50)
+      ]],
+      email: ['', [Validators.required,
+        Validators.email,
+        Validators.maxLength(50)
+      ]],
+      password: ['', [Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(50)
+      ]],
+      rePassword: ['', [Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(50)
+      ]],
+      userName: ['', [Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(50)
+      ]],
+    },{validators:(group:AbstractControl):ValidationErrors | null=>{
+      let password=group.get("password").value;
+      let rePassword=group.get("rePassword").value;
+      return password===rePassword?null:{notSame:true};
+      }})
+  }
+
+  get component() {
+    return this.frm.controls;
+  }
+
+
+  isSubmitted: boolean = false;
+
+  onSubmit(values: any) {
+    this.isSubmitted = true;
+
+    if (this.frm.invalid)
+      return;
+
+  }
 }
--- UlukunShop.UI/src/app/ui/components/register/register.module.ts
+++ UlukunShop.UI/src/app/ui/components/register/register.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RegisterComponent} from "./register.component";
 import {RouterModule} from "@angular/router";
+import {ReactiveFormsModule} from "@angular/forms";
 
 
 
@@ -11,7 +12,9 @@ import {RouterModule} from "@angular/router";
   ],
   imports: [
     CommonModule,
-    RouterModule.forChild([{path:'',component:RegisterComponent}])
+    RouterModule.forChild([{path:'',component:RegisterComponent}
+    ]),
+    ReactiveFormsModule
   ]
 })
 export class RegisterModule { }
