5a079e007e89f980ddee2b1dbf6f8976e544eecb
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-04-14 23:19:54 +0300

    Cilalama devam.

--- UlukunShop.UI/src/app/ui/components/components.module.ts
+++ UlukunShop.UI/src/app/ui/components/components.module.ts
@@ -10,6 +10,7 @@ import {ContactModule} from "./contact/contact.module";
 import { UiFooterComponent } from './ui-footer/ui-footer.component';
 import { ThankyouComponent } from './thankyou/thankyou.component';
 import {ThankyouModule} from "./thankyou/thankyou.module";
+import {RouterLinkWithHref} from "@angular/router";
 
 
 
@@ -21,14 +22,15 @@ import {ThankyouModule} from "./thankyou/thankyou.module";
   exports: [
     UiFooterComponent
   ],
-  imports: [
-    CommonModule,
-    ProductsModule,
-    ShoppingcartsModule,
-    HomeModule,
-    RegisterModule,
-    ContactModule,
-    ThankyouModule
-  ]
+    imports: [
+        CommonModule,
+        ProductsModule,
+        ShoppingcartsModule,
+        HomeModule,
+        RegisterModule,
+        ContactModule,
+        ThankyouModule,
+        RouterLinkWithHref
+    ]
 })
 export class ComponentsModule { }
